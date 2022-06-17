cbe668b717ab731d2144af85ba7372f85774e46a
Sanghun <po4865@gmail.com>
2022-06-17 15:36:44 +0200

    Initial Commit

---
+++ TeaPlz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TeaPlz
+//
+//  Created by Sanghun Park on 17.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TeaPlz/TeaPlzApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeaPlzApp.swift
+//  TeaPlz
+//
+//  Created by Sanghun Park on 17.06.22.
+//
+
+import SwiftUI
+
+@main
+struct TeaPlzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
