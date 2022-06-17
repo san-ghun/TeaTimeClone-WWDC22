39bc59f6518c51e8fbac95729cbfda2e0f2411ca
Sanghun <po4865@gmail.com>
2022-06-17 15:49:42 +0200

    init: Initiate TeaPlz SwiftUI project

---
+++ TeaPlz/Model/ModelHelpers.swift
@@ -0,0 +1,8 @@
+//
+//  ModelHelpers.swift
+//  TeaPlz
+//
+//  Created by Sanghun Park on 17.06.22.
+//
+
+import Foundation
---
+++ TeaPlz/Model/Tea.swift
@@ -0,0 +1,8 @@
+//
+//  Tea.swift
+//  TeaPlz
+//
+//  Created by Sanghun Park on 17.06.22.
+//
+
+import Foundation
