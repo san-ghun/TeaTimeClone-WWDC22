f48ff9d6e3e874a70762f5d198fe93c44b6f062b
Sanghun <po4865@gmail.com>
2022-06-18 18:03:15 +0200

    fix: Update ContentView.swift for incorrect arguement

--- TeaPlz/ContentView.swift
+++ TeaPlz/ContentView.swift
@@ -32,7 +32,7 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             ContentView()
-                .previewInterfaceOrientation(.portraitUpsideDown)
+                .previewInterfaceOrientation(.portrait)
         }
     }
 }
